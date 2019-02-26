36301e51c9ef78a2cbdeccef35855b9517dfcfe1
Sazz <bladedge666@gmail.com>
2019-02-26 13:47:54 -0500

    Cleaned spaces

--- myftp.java
+++ myftp.java
@@ -1,155 +1,148 @@
-//updated Project 2
-​
 
+//updated Project 2
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;​
+import java.util.Scanner;
 
 /**
  * This is the FTP client for the project. On executing this file, you are given
  * a "myftp>" prompt where you can type the available commands.
  */
 public class myftp {
-  ​
+
   private final static String FILE_SEP = System.getProperty("file.separator");
   private final static int BUFFER_SIZE = 1024;
   private static final Scanner scan = new Scanner(System.in);
   public static String commandID;
 
- public static void main(String[] args) throws IOException, ClassNotFoundException {
-​
-  int port = Integer.parseInt(args[0]);
-  String machineName = args[1];
-  Socket server = new Socket(machineName, port);
-  String splitCommand[]=null;
-  DataOutputStream output = new DataOutputStream(server.getOutputStream());
-  DataInputStream input = new DataInputStream(new BufferedInputStream(server.getInputStream()));
-  ObjectInputStream objInput = new ObjectInputStream(server.getInputStream());
-  //ObjectOutputStream objOutput = new ObjectOutputStream(server.getOutputStream());
-​
-  //FileOutputStream fileOutStream;
-  //FileInputStream fileInStream;
-  
-​
-  boolean quit = false;
-  while (!quit) {
-   output.flush();
-   // outStream.flush();
-   System.out.print("myftp> ");
-   String command = scan.nextLine();
-   output.writeUTF(command);
-   int cmdLength = command.split(" ").length;
-   splitCommand=command.split("\\s+");
-   
-​
-   if (command.equals("ls")) {
-    String listOfFiles[] = (String[]) objInput.readObject();
-    for (int i = 0; i < listOfFiles.length; i++) {
-     System.out.printf("%s \t", listOfFiles[i]);
-    }
-    System.out.println();
-​
-   }
-​
-   else if (command.equals("quit")) {
-    server.close();
-    quit = true;
-   }
-​
-   else if ((cmdLength == 2 || cmdLength==3) && splitCommand[0].equals("get")) {
-  	 if(command.endsWith("&")) {
-  		 new getAndPutWithAmpersand(server,splitCommand[0],input,splitCommand[1],output);
-  	 }else {
-    	 get(input,splitCommand[1],output);
-  	 }
-  	 try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-   }
-​
-   else if ((cmdLength == 2 || cmdLength==3 ) && splitCommand[0].equals("put")) {
-  	 if(command.endsWith("&")) {
-  		 new getAndPutWithAmpersand(server,splitCommand[0],input,splitCommand[1],output);
-  	 }else {
-  		 put(output,input,splitCommand[1]);
-  	 }try {
- 			Thread.sleep(500);
- 		} catch (InterruptedException e) {
- 			// TODO Auto-generated catch block
- 			e.printStackTrace();
- 		}
-  	 
-   }
-   else if(cmdLength==2 && splitCommand[0].equals("terminate")){
-  	 terminate(command);
-   }
-   else {
-    System.out.println(input.readUTF());
-   }
-​
-  } // end while
-​
- }​
+  public static void main(String[] args) throws IOException, ClassNotFoundException {
 
- static void put(DataOutputStream output,DataInputStream input,String command) {
-	 try {
-		 //output.writeUTF(System.getProperty("user.dir"));
-		 //System.out.println(System.getProperty("user.dir"));
-		 File sendFile = new File(command);
-	   output.writeUTF(String.valueOf(sendFile.exists()));
-	   commandID=input.readUTF();
-	   System.out.println("CommandID "+commandID);
-	   if (sendFile.exists()) {
-	    FileInputStream fileInStream = new FileInputStream(sendFile.getAbsolutePath());
-//	    System.out.println(fileInStream.available());
-	    output.writeLong(sendFile.length());
-	    byte[] putByteArray = new byte[(int) sendFile.length()];
-​
-	    DataInputStream dis = new DataInputStream(fileInStream);
-	    boolean terminated=false;
-	    int pointer=0,readChars;
-	    int fileSize=putByteArray.length;
-	    
-//	    if(NewThread.runningCommands.contains(commandID)) {
-    		for(;pointer<fileSize;pointer+=readChars) {
-    			if((fileSize-pointer)>1000) {
-    				readChars=1000;
-    			}else {
-    				readChars=1;
-    			}
-    			dis.read(putByteArray, pointer, readChars); // readfully is required to read all at once
-    			output.write(putByteArray, pointer, readChars); //
-    			//System.out.println(readChars+"   "+pointer);
-//    			if(readChars%1000==0) {
-//	    			if(!NewThread.runningCommands.contains(commandID)) {
-//	    				pointer=fileSize;
-//	    				terminated=true;
-//	    				break;
-//	    	    }
-//    			}
-​
-    		}
-//    	}	
-//    	else {
-//    		terminated=true;
-//    	}
-	    
-	    output.flush();
-//	    System.out.println("File successfully uploaded on server!");
-	    dis.close();
-	    //input.readUTF();
-	   } else {
-	    System.out.println("File does not exist!");
-	   }
-	 }
-	 catch(Exception e) {
-		 System.out.println(e);
-	 }
- }
+    int port = Integer.parseInt(args[0]);
+    String machineName = args[1];
+    Socket server = new Socket(machineName, port);
+    String splitCommand[] = null;
+    DataOutputStream output = new DataOutputStream(server.getOutputStream());
+    DataInputStream input = new DataInputStream(new BufferedInputStream(server.getInputStream()));
+    ObjectInputStream objInput = new ObjectInputStream(server.getInputStream());
+    // ObjectOutputStream objOutput = new
+    // ObjectOutputStream(server.getOutputStream());
+
+    boolean quit = false;
+    while (!quit) {
+      output.flush();
+      // outStream.flush();
+      System.out.print("myftp> ");
+      String command = scan.nextLine();
+      output.writeUTF(command);
+      int cmdLength = command.split(" ").length;
+      splitCommand = command.split("\\s+");
+
+      if (command.equals("ls")) {
+        String listOfFiles[] = (String[]) objInput.readObject();
+        for (int i = 0; i < listOfFiles.length; i++) {
+          System.out.printf("%s \t", listOfFiles[i]);
+        }
+        System.out.println();
+
+      }
+
+      else if (command.equals("quit")) {
+        server.close();
+        quit = true;
+      }
+
+      else if ((cmdLength == 2 || cmdLength == 3) && splitCommand[0].equals("get")) {
+        if (command.endsWith("&")) {
+          new getAndPutWithAmpersand(server, splitCommand[0], input, splitCommand[1], output);
+        } else {
+          get(input, splitCommand[1], output);
+        }
+        try {
+          Thread.sleep(500);
+        } catch (InterruptedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+
+      else if ((cmdLength == 2 || cmdLength == 3) && splitCommand[0].equals("put")) {
+        if (command.endsWith("&")) {
+          new getAndPutWithAmpersand(server, splitCommand[0], input, splitCommand[1], output);
+        } else {
+          put(output, input, splitCommand[1]);
+        }
+        try {
+          Thread.sleep(500);
+        } catch (InterruptedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+
+      } else if (cmdLength == 2 && splitCommand[0].equals("terminate")) {
+        terminate(command);
+      } else {
+        System.out.println(input.readUTF());
+      }
+
+    } // end while
+
+  }
+
+  static void put(DataOutputStream output, DataInputStream input, String command) {
+    try {
+      // output.writeUTF(System.getProperty("user.dir"));
+      // System.out.println(System.getProperty("user.dir"));
+      File sendFile = new File(command);
+      output.writeUTF(String.valueOf(sendFile.exists()));
+      commandID = input.readUTF();
+      System.out.println("CommandID " + commandID);
+      if (sendFile.exists()) {
+        FileInputStream fileInStream = new FileInputStream(sendFile.getAbsolutePath());
+        // System.out.println(fileInStream.available());
+        output.writeLong(sendFile.length());
+        byte[] putByteArray = new byte[(int) sendFile.length()];
+
+        DataInputStream dis = new DataInputStream(fileInStream);
+        boolean terminated = false;
+        int pointer = 0, readChars;
+        int fileSize = putByteArray.length;
+
+        // if(NewThread.runningCommands.contains(commandID)) {
+        for (; pointer < fileSize; pointer += readChars) {
+          if ((fileSize - pointer) > 1000) {
+            readChars = 1000;
+          } else {
+            readChars = 1;
+          }
+          dis.read(putByteArray, pointer, readChars); // readfully is required to read all at once
+          output.write(putByteArray, pointer, readChars); //
+          // System.out.println(readChars+" "+pointer);
+          // if(readChars%1000==0) {
+          // if(!NewThread.runningCommands.contains(commandID)) {
+          // pointer=fileSize;
+          // terminated=true;
+          // break;
+          // }
+          // }
+
+        }
+        // }
+        // else {
+        // terminated=true;
+        // }
+
+        output.flush();
+        // System.out.println("File successfully uploaded on server!");
+        dis.close();
+        // input.readUTF();
+      } else {
+        System.out.println("File does not exist!");
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
 
   static void get(DataInputStream input, String command, DataOutputStream output) {
     try {
@@ -191,7 +184,7 @@ public class myftp {
     server.close();
   }
 
-}​
+}
 
 class getAndPutWithAmpersand implements Runnable {
   Thread getPutThread;
